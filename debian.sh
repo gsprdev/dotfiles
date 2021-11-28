57512d1626202cc7faed384d36e448cd35517452
Gary Spreder <garypreder@gmail.com>
2021-11-28 20:34:36 +0000

    From Debian, remove Kitty, add Tmux

--- debian.sh
+++ debian.sh
@@ -11,7 +11,7 @@ function configure() {
     while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
     # Install required software
-    sudo apt-get -y install vim git zsh tmux xclip kitty
+    sudo apt-get -y install vim git zsh tmux xclip rsync
 
     # Use ZSH
     sudo chsh -s $(which zsh) $(whoami)
